d2eebb87ea99510046c77beeae523f493b99c9de
ariAN <114738229+arian13momeni@users.noreply.github.com>
2022-12-13 18:54:07 +0330

    Create menue.py

---
+++ menue.py
@@ -0,0 +1,17 @@
+from pyfiglet import *
+print(figlet_format("hack eita "))
+
+text = figlet_format("hack eita")
+print("macke whit love and Coffee ")
+print("Welcome to the eita hack")
+print("------------------------------------------------------------------------------------");
+print("1.chanell hack")
+print("2. member hack")
+print("\n")
+choose = input("Choose an option:")
+
+if (str(choose) == "1"):
+   print(figlet_format("hack chanell"))
+elif (str(choose) == "2"):
+    print(figlet_format("hackmember"))
+
